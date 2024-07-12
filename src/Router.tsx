0ad25891d2da51428762a7f18e9be2250055c23e
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-07-13 03:42:45 +0530

    update router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Homepage from "./page/Homepage";
 import About from "./component/About";
 import SerachFilterData from "./component/SerachFilterData";
 
 const AppRouter = () => (
-  <HashRouter basename="/RealState-github">
+  <BrowserRouter basename="/RealState-github">
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/:userData" element={<SerachFilterData />} />
       <Route path="/about/:userAbout" element={<About />} />
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default AppRouter;
